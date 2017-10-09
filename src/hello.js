ff399ba26e2d2b3f915533b89d10c0218baf8c75
yanxiaodi <929213769@qq.com>
2017-10-09 20:29:58 +0800

    :art: 配置完成 Flow

--- src/hello.js
+++ src/hello.js
@@ -11,6 +11,6 @@ function square (n: number): number {
   return n * n
 }
 
-square(2) // 检查 flow 纠错功能 => 成功发现 类型错误!
+square('2') // 检查 flow 纠错功能 => 成功发现 类型错误!
 
 // let a = '1'; // 检查 eslint - standard 纠错功能 => 成功发现 语法错误!
